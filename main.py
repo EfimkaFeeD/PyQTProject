11e9d71e2f3f21ae3d15f00cf17ab369c592404c
efimkafeed <efimkafeed@gmail.com>
2022-11-05 21:18:10 +0300

    added about program dialog with txt support, added half working fancy printing from database, changed database a little, made gui slightly better

--- main.py
+++ main.py
@@ -1,34 +1,73 @@
 import sqlite3
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QDialog
 
 
-class MyApp(QMainWindow):
+class MainApp(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.connection = sqlite3.connect('films_db.db')
         self.searchbutton.clicked.connect(self.search_in_table)
+        self.about_program_action.triggered.connect(
+            lambda: AboutProgramDialog().exec_())
 
     def search_in_table(self):
+        result = []
         query = self.searchedit.text()
-        result = self.connection.cursor().execute("""SELECT ? FROM films_table""", (query, )).fetchall()
-        self.tableWidget.setColumnCount(19)
-        self.tableWidget.setRowCount(0)
-        for i, row in enumerate(result):
-            self.tableWidget.setRowCount(
-                self.tableWidget.rowCount() + 1)
-            for j, elem in enumerate(row):
-                self.tableWidget.setItem(
-                    i, j, QTableWidgetItem(str(elem)))
+        tmp = self.connection.cursor().execute(
+            """SELECT * FROM films_table WHERE title = ?""", (query, )).fetchall()
+        tmp = tmp[0]
+        for i in tmp:
+            result.append(i)
+        print(result)
+        # TODO make database linking ids work
+        # TODO make description, poster, images work
+        # TODO do smth with seasons and episodes
+        # TODO right now trailer will be just youtube link, make embedded player or connect youtube-dlp
+        # TODO make it look prettier
+        self.searchresult.setFontPointSize(24)
+        try:
+            self.searchresult.setText(f"""
+            Номер в базе данных: {result[0]}
+            Название: {result[1]}
+            Жанр: {result[2]}
+            Тип кино: {result[3]}
+            Дата премьеры: {result[4]}
+            Продолжительность в минутах: {result[5]}
+            Описание: {result[6]}
+            Постер: {result[7]}
+            Изображения: {result[8]}
+            Трейлер: {result[9]}
+            Режиссер и съемочная команда: {result[10]} / {result[12]}
+            Актеры: {result[11]}
+            Количество сезонов / серий: {result[13]} / {result[14]}
+            Рейтинг на IMDb: {result[15]} из 10
+            Рейтинг на Rotten Tomatoes: {result[16]}% из 100%
+            Рейтинг на КиноПоиск: {result[17]} из 10
+            Где смотреть: {result[18]}
+            """)
+        except IndexError:
+            self.searchresult.setText(
+                'По вашему запросу не было найдено информации, либо информация по данному запросу некорректна. '
+                'Попробуйте изменить свой запрос или отредактируйте некорректную информацию.')
 
     def closeEvent(self, event):
         self.connection.close()
 
 
+class AboutProgramDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('about_program.ui', self)
+        with open('about_program.txt') as file:
+            txt = file.read()
+        self.about_text.setText(txt)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyApp()
+    ex = MainApp()
     ex.show()
     sys.exit(app.exec_())
